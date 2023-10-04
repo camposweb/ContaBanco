fb1ab1b46bd82a38674f44c8b61120c838e9c41c
Leandro Campos <camposweb@gmail.com>
2023-10-04 15:31:13 -0300

    desafio completo!

--- src/main/java/ContaTerminal.java
+++ src/main/java/ContaTerminal.java
@@ -1,2 +1,42 @@
 public class ContaTerminal {
+
+    private int conta;
+    private String agencia;
+    private String nomeCliente;
+    private double saldo;
+
+    public ContaTerminal() {
+    }
+
+    public int getConta() {
+        return conta;
+    }
+
+    public void setConta(int conta) {
+        this.conta = conta;
+    }
+
+    public String getAgencia() {
+        return agencia;
+    }
+
+    public void setAgencia(String agencia) {
+        this.agencia = agencia;
+    }
+
+    public String getNomeCliente() {
+        return nomeCliente;
+    }
+
+    public void setNomeCliente(String nomeCliente) {
+        this.nomeCliente = nomeCliente;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,21 @@
+import java.util.Scanner;
+
+
 public class Main {
     public static void main(String[] args) {
 
-
+        ContaTerminal cliente = new ContaTerminal();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite o número da Agência: ");
+        cliente.setAgencia(scanner.next());
+        System.out.println("Digite o número da Conta: ");
+        cliente.setConta(scanner.nextInt());
+        System.out.println("Digite o nome do cliente: ");
+        cliente.setNomeCliente(scanner.next());
+        System.out.println("Digite o saldo: ");
+        cliente.setSaldo(scanner.nextDouble());
+        System.out.println("Olá " + cliente.getNomeCliente() + " obrigado por criar uma conta em nosso banco, " +
+                "sua agência é " + cliente.getAgencia() + " conta " + cliente.getConta() + " e seu saldo "
+                + cliente.getSaldo() + " já está disponível para saque" );
     }
 }
